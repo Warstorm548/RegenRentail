920c1b447a75f7b96a9d19c960da72d92855eeef
JeanLuc <jjwatt642@gmail.com>
2025-11-30 23:51:00 -0500

    fix: Add missing List import to SignManager

--- src/main/java/com/regionrental/managers/SignManager.java
+++ src/main/java/com/regionrental/managers/SignManager.java
@@ -14,6 +14,7 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.WallSign;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
